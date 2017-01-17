50a6f19ee6de5e4cee5d3963e8f2917bbcc0915f
unisparkAdmin <tony@unispark.co>
2017-01-17 11:32:46 -0800

    Added Triangulation logic

--- Swarm/Assets/code/SwarmMember.cs
+++ Swarm/Assets/code/SwarmMember.cs
@@ -7,7 +7,12 @@ public class SwarmMember : MonoBehaviour {
 	private float thrust = 9.6f;
 	private Rigidbody rb;
 	private int verticalDistance;
-	private ArrayList closestMembers;
+	private float idealNeightorDistance = 100f;
+	private float idealNeighborDistanceThreshold = 5f;
+
+	// Each Member constantly triangulates with its closest 2 Members.
+	private SwarmMember closestMember0;
+	private SwarmMember closestMember1;
 
 
 	// Use this for initialization
@@ -15,12 +20,17 @@ public class SwarmMember : MonoBehaviour {
 		rb = GetComponent<Rigidbody> ();
 		rb.useGravity = true;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
+		
 		changeVector ();
 		maintainFlight ();
+
+	}
+
+	public void addClostestMember(SwarmMember newMember)
+	{
 
 	}
 
@@ -30,8 +40,27 @@ public class SwarmMember : MonoBehaviour {
 
 	}
 
+	bool isWithinDistanceThreshold(SwarmMember member)
+	{
+		//Vector3.Distance(rb.position, closestMember0.rb.position
+	}
+
+	// Stay equal distance from the closest 2 members and stay in the air.
 	void maintainFlight()
 	{
+		// Check distnace between this and the first RigidBody
+		if(isWithinDistanceThreshold(closestMember0))
+		{
+
+		}
+
+		// Check distnace between this and the second RigidBody
+		if(isWithinDistanceThreshold(closestMember1))
+		{
+
+		}
+
+		// stay up
 		rb.AddForce(0,thrust,0, ForceMode.Force);
 	}
 }
