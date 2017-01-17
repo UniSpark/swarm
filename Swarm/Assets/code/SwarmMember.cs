85395bb5ab69e02e818212beca64ed2589f24c15
unisparkAdmin <tony@unispark.co>
2017-01-16 18:22:37 -0800

    added physics

--- Swarm/Assets/code/SwarmMember.cs
+++ Swarm/Assets/code/SwarmMember.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SwarmMember : MonoBehaviour {
 
-	private float thrust = 9.8f;
+	private float thrust = 9.7f;
 	private Rigidbody rb;
 	private int verticalDistance;
 	private ArrayList closestMembers;
